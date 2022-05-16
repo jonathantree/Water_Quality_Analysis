27958714b3ba67373743b081eb815001413f80ce
Jonathan Tree <46392976+jonathantree@users.noreply.github.com>
2022-05-16 16:48:17 -0700

    Adding sqlite database for final data storage

--- Database/schema.sql
+++ Database/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE utility_zips(
 	"People served"	TEXT,
 	"Zip" INT
 )
+
+CREATE TABLE Utilities_TEST (
+    Utility            TEXT    NOT NULL,
+    Contaminant_Factor INTEGER NOT NULL,
+    Zip                INTEGER NOT NULL,
+    county_FIPS        INTEGER REFERENCES FIPS_Codes (fips) 
+);
