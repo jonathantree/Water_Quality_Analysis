930593467688136b2ef955b8eec87035687598bd
Jenna Dodge <94572628+jennadodge@users.noreply.github.com>
2022-05-23 12:53:57 -0600

    handled outliers for contaminant data

--- Data_Summary_Statistics/Ordinal_Logistic_Regression_Practice.R
+++ Data_Summary_Statistics/Ordinal_Logistic_Regression_Practice.R
@@ -1,10 +1,53 @@
-library(carData)
-library(MASS)
+require(foreign)
+require(ggplot2)
+require(MASS)
+require(Hmisc)
+require(reshape2)
 
-data(WVS)
-head(WVS)
-summary(WVS)
+dat <- read.dta("https://stats.idre.ucla.edu/stat/data/ologit.dta")
+head(dat)
 
-ggplot2(WVS,aes(x = poverty, y=age, fill = poverty)) + 
-  geom_boxplot(size=0.75) + facet_grid(country ~ gender, margins = FALSE) +
-   theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1))
+# Outcome variable: unlikely, somewhat likely, or very likely to apply (coded 1, 2, 3 respecively)
+# Pared - 0/1 indicates if at least one parent has a graduate degree
+# public (1 public, 0 private)
+# gpa 
+
+# one at a time apply table to 
+lapply(dat[, c("apply", "pared", "public")], table)
+
+## three way cross tabs (xtabs) and flatten the table
+ftable(xtabs(~ public + apply + pared, data = dat))
+
+summary(dat$gpa)
+sd(dat$gpa)
+
+ggplot(dat, aes(x = apply, y = gpa)) +
+  geom_boxplot(size = .75) +
+  geom_jitter(alpha = .5) +
+  facet_grid(pared ~ public, margins = TRUE) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+## fit ordered logit model and store results 'm'
+m <- polr(apply ~ pared + public + gpa, data = dat, Hess=TRUE)
+
+## view a summary of the model
+summary(m)
+
+## store table
+(ctable <- coef(summary(m)))
+
+## calculate and store p values
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+
+## combined table
+(ctable <- cbind(ctable, "p value" = p))
+
+(ci <- confint(m)) # default method gives profiled CIs
+
+confint.default(m) # CIs assuming normality
+
+## odds ratios
+exp(coef(m))
+
+## OR and CI - Proponal Odds Ratios
+exp(cbind(OR = coef(m), ci))
