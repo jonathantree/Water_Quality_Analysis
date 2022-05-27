b8469252324fbaea0f08306975ba6353246f6842
Jenna Dodge <94572628+jennadodge@users.noreply.github.com>
2022-05-26 18:46:53 -0600

    rearranging files to create repository/submodule for test app deployment

--- Dashboard/Dash_Jenna/dash_selenium_test_local.py
+++ Dashboard/Dash_Jenna/dash_selenium_test_local.py
@@ -15,6 +15,7 @@ from urllib.request import urlopen
 import json
 
 app = Dash(__name__,
+                
                 external_stylesheets=[dbc.themes.SANDSTONE],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
